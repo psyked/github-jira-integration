eb1349216664f480971b170d26674682d764fa03
James Ford <james.ford@bglgroup.co.uk>
2018-10-26 21:07:34 +0100

    Refactoring

--- src/utils/octokit-helpers/index.js
+++ src/utils/octokit-helpers/index.js
@@ -1,8 +1,8 @@
 // @flow
 const octokit = require("@octokit/rest")();
-const base64 = require("base-64");
-const yaml = require("js-yaml");
+
 const log = require("../logger");
+const getConfigFromRepo = require("./getConfigFromRepo");
 
 const { GITHUB_TOKEN, JIRA_BASE_URL } = require("../../config");
 
@@ -10,37 +10,6 @@ octokit.authenticate({
     type: "token",
     token: GITHUB_TOKEN
 });
-
-/**
- * Reads & returns the `github-jira` confiration file.
- */
-const getConfigFromRepo = async ({ owner, repo } = {}) => {
-    try {
-        const githubConfig = await octokit.repos.getContent({
-            owner,
-            repo,
-            path: ".github/github-jira.yml"
-        });
-
-        const { data: { content } = {} } = githubConfig || {};
-
-        const yml = base64.decode(content);
-
-        const config = yaml.safeLoad(yml);
-
-        log.info(
-            {
-                owner,
-                repo
-            },
-            "Successfully got config from project"
-        );
-
-        return config;
-    } catch (err) {
-        throw new Error("Failed to get `github-jira.yml` file");
-    }
-};
 
 const createGithubComment = async ({ owner, repo, number, body }) => {
     try {
